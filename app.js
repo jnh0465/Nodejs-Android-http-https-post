90e741f4a1214d846fb30ca9e38b09619f3e6b7d
jiwoolee <jnh0465@naver.com>
2018-09-26 00:10:03 +0900

    first: Node.js code to perform POST

---
+++ app.js
@@ -0,0 +1,35 @@
+// --> npm install express --save
+// --> npm install ejs --save
+
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var fs = require('fs');
+
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+var app_uuid, inputData;
+app.post("/post", (req, res) => {
+   req.on("data", (data) => {
+     inputData = JSON.parse(data);
+   });
+   req.on("end", () => {
+     app_uuid= inputData.UUID;
+     console.log("UUID : "+inputData.UUID);
+   });
+   res.end();
+});
+
+app.get("/users", (req, res) => {
+  res.render(__dirname+'/public/index.html',{          // to index.html
+    'uuid' : app_uuid
+  });
+});
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+app.listen(3000, function(){
+  console.log("Express server has started on port 3000");
+});
